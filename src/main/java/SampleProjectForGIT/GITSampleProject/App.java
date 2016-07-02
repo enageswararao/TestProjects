90411757094e14912cb4f87e226bbaa17fa62815
Nageshwara Rao Eslavath <neslavath@csilhyd1.com>
2016-07-03 01:11:02 +0530

    initial upload

---
+++ src/main/java/SampleProjectForGIT/GITSampleProject/App.java
@@ -0,0 +1,13 @@
+package SampleProjectForGIT.GITSampleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SampleProjectForGIT/GITSampleProjectTest/ApplicationTest.java
@@ -0,0 +1,28 @@
+package SampleProjectForGIT.GITSampleProjectTest;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ApplicationTest {
+
+	
+	@BeforeTest public void init(){
+		System.out.println("Before Test methods is init () ...");
+	}
+
+	@Test public void tc_001_TestCase1(){
+		System.out.println("tc_001 TestCase1.......");
+	}
+	
+	@Test public void tc_002_TestCase2(){
+		System.out.println("tc_002 TestCase2.......");
+	}
+	
+	@Test public void tc_003_TestCase3(){
+		System.out.println("tc_003 TestCase3.......");
+	}
+	@AfterTest public void close(){
+		System.out.println("@AfterTest Close.......");
+	}
+}
